359e6d67772563ef52fd55882290ece3b3206f48
Jonathan LeBlanc <joncleblanc@yahoo.com>
2017-08-15 10:33:20 -0500

    Added structured comments

--- java/samples/box-sdk-samples/src/main/java/Main.java
+++ java/samples/box-sdk-samples/src/main/java/Main.java
@@ -4,28 +4,34 @@ import com.box.sdk.BoxFolder;
 import com.box.sdk.BoxItem;
 
 public class Main extends Config {
-	public static void main(String[] args) {    	
-        get("/start", (req, res) -> {
-	        String box_redirect = "https://account.box.com/api/oauth2/authorize?"
+	public static void main(String[] args) { 
+		get("/start", (req, res) -> {
+			// Redirect user to login with Box
+			String box_redirect = "https://account.box.com/api/oauth2/authorize?"
 	        		+ "response_type=code"
 	        		+ "&client_id=" + Config.client_id 
 	        		+ "&redirect_uri=" + Config.redirect_uri;
 	        	res.redirect(box_redirect);
 	        	
-	        	return "hello";
+	        	return "Redirecting...";
         });
         
+        // 
         get("/return", (req, res) -> {
+        		// Capture auth code 
         		String code = req.queryParams("code");  
         		
+        		// Instantiate new Box API connection object
         		BoxAPIConnection api = new BoxAPIConnection(Config.client_id, Config.client_secret, code);
         		
+        		
+        		// Upload new file
         		BoxFolder rootFolder = BoxFolder.getRootFolder(api);
 	        	for (BoxItem.Info itemInfo : rootFolder) {
 	        	    System.out.format("[%s] %s\n", itemInfo.getID(), itemInfo.getName());
 	        	}
         	
-        	    return "response";
+        	    return "Display page";
         });
     }
 }
--- java/samples/box-sdk-samples/src/main/java/jwt/JWTAuth.java
+++ java/samples/box-sdk-samples/src/main/java/jwt/JWTAuth.java
@@ -16,6 +16,7 @@ public class JWTAuth extends Config {
 	private static final int MAX_CACHE_ENTRIES = 100;
 	
 	public static void main(String[] args) throws IOException { 
+		// Read private key file
 		File file = new File(Config.private_key);
         byte[] fileData = new byte[(int) file.length()];
         DataInputStream dis = new DataInputStream(new FileInputStream(file));
@@ -24,17 +25,19 @@ public class JWTAuth extends Config {
 
         String privateKey = new String(fileData);
 
+        // Set up private key encryption info
         JWTEncryptionPreferences encryptionPref = new JWTEncryptionPreferences();
         encryptionPref.setPublicKeyID(Config.jwt_public_key_id);
         encryptionPref.setPrivateKey(privateKey);
         encryptionPref.setPrivateKeyPassword(Config.jwt_private_key_password);
         encryptionPref.setEncryptionAlgorithm(EncryptionAlgorithm.RSA_SHA_256);
 		
+        // Create new API connection
 		IAccessTokenCache accessTokenCache = new InMemoryLRUAccessTokenCache(MAX_CACHE_ENTRIES);
-		
 		BoxDeveloperEditionAPIConnection api = BoxDeveloperEditionAPIConnection.getAppEnterpriseConnection(Config.enterprise_id, Config.client_id, Config.client_secret, encryptionPref, accessTokenCache);
 
 		// Create new app user
-		BoxUser.Info createdUserInfo = BoxUser.createAppUser(api, "New User");
+		BoxUser.Info createdUserInfo = BoxUser.createAppUser(api, "New User 123");
+		System.out.println("User created");
 	}
 }
