ce32576a0cb135357be8177b94040922d53aaf2d
Jonathan LeBlanc <joncleblanc@yahoo.com>
2017-08-21 15:15:50 -0700

    Add representations API sample (JWT)

---
+++ python/representations_watermark/config_jwt.py
@@ -0,0 +1,12 @@
+# Default search values
+user_name = 'John Snow'
+file_name = 'confidential.pdf'
+folder_id = '34282719782'
+
+# Auth config
+client_id = '8ivdviqhsc14bdh6sezozg3vbqns2sfd'
+client_secret='2zD1zjCICi6u9qTrTGYaZMKULg8UUZ5I'
+enterprise_id='17404983'
+jwt_key_id= '8tuucw4g'
+private_key_path='private.pem'
+private_key_passphrase='e3kbsa'
